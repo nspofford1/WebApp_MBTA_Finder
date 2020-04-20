0a222169a3c80d6b459a6e37e692308ed9a8967b
nspofford1 <nspofford1@babson.edu>
2020-04-19 22:40:26 -0400

    Final Project

--- app.py
+++ app.py
@@ -1,21 +1,15 @@
 from flask import Flask, render_template, request
-
-from mbta_finder import find_stop_near
+from mbta_helper import *
 
 app = Flask(__name__)
 
-
-
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/", methods=["GET","POST"])
 def find():
-    # modify this function so it renders different templates for POST and GET method.
-    # aka. it displays the form when the method is 'GET'; it displays the results when
-    # the method is 'POST' and the data is correctly processed.
-    if request.method == 'POST':
-        ...
-    else:
-        ...
-
-
-
-
+    if request.method == "POST":
+        location = str(request.form["location"])
+        MBTA_Station, wheelchair = find_stop_near(location)
+        if location:
+            return render_template("mbta_result.html", location = location, MBTA_Station = MBTA_Station, wheelchair = wheelchair)
+        else:
+            return render_template('index.html')
+    return render_template('index.html')
